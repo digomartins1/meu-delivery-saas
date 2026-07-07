abfb0c1647c3022a7744761e9b8ce3a52af4f8c6
digomartins1 <85768517+digomartins1@users.noreply.github.com>
2026-07-07 05:05:05 -0300

    Refactor get_db function for concise syntax

--- app/database.py
+++ app/database.py
@@ -3,21 +3,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# 1. Pega a URL que você vai colocar no Render
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./delivery.db")
-
-# 2. Corrige o link para o formato que o Python aceita
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-# 3. Conecta ao Banco de Dados
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+    try: yield db
+    finally: db.close()
