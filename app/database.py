09d7c5840b07d6f4fd3ca9db3a45aa43c7623463
digomartins1 <85768517+digomartins1@users.noreply.github.com>
2026-07-07 04:41:47 -0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -3,15 +3,21 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_URL = os.getenv("DATABASE_URL", "sqlite:///./delivery.db")
-if DB_URL.startswith("postgres://"):
-    DB_URL = DB_URL.replace("postgres://", "postgresql://", 1)
+# 1. Pega a URL que você vai colocar no Render
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./delivery.db")
 
-engine = create_engine(DB_URL)
+# 2. Corrige o link para o formato que o Python aceita
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+# 3. Conecta ao Banco de Dados
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
-    try: yield db
-    finally: db.close()
+    try:
+        yield db
+    finally:
+        db.close()
